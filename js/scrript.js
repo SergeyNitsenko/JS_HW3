5fb23203d54e45826ce2eb84f292e4e80a993200
SergeyNitsenko <s.erg-86@mail.ru>
2023-03-09 18:05:26 +0300

    add: js_hw3

---
+++ js/scrript.js
@@ -0,0 +1,139 @@
+"use stict"
+
+
+//Task 1
+
+{
+    // let i = 1;
+
+    // while (i <= 50) {
+    //     console.log(i);
+    //     i++;
+    // }
+
+    // i = 35;
+
+    // while (i >= 8) {
+    //     console.log(i)
+    //     i--;
+    // }
+}
+
+//Task 2 
+
+{
+    //  let i = 89;
+
+    // // while (i >= 11) {
+    // //     console.log(i);
+    // //     i--;
+    // // }
+
+    // for (; i >= 11; i--) console.log(i)
+}
+
+
+//Task 3
+
+
+{
+    // let a = 100;
+    // let sum = 0;
+
+    // for (let i = 0; i <= a; i++) {
+    //     sum += i;
+    // }
+    // console.log(sum)
+}
+
+
+//Task 4
+
+{
+    // let a = 5;
+    // let sum = 0;
+
+    // for (let i = 0; i <= a; i++) {
+    //     sum += i;
+    //     sum = 0;
+    //     for (let j = 1; j <= i; j++) {
+    //         sum += j;
+    //     }
+    //     console.log('Sum ' + i + ' = ' + sum);
+    // }
+
+}
+
+
+
+
+//Task 5 
+{
+    // for (let i = 8; i <= 56; i++) {
+    //     if (i % 2 === 0)
+    //         console.log(i);
+    // }
+}
+
+
+
+//Task 6
+
+{
+    // for (let j = 2; j <= 10; j++) {
+    //     console.log(j);
+    //     for (let i = 1; i <= 10; i++) {
+    //         console.log(`${i} * ${j}  = ${i*j}`)
+    //     }
+    // }
+}
+
+
+// Task 7
+
+{
+    // let num = 0;
+    // let n;
+
+    // for (n = 1000; n >= 50; n /= 2) {
+    //     num++;
+    // }
+    // console.log(`Число ${n}`);
+    // console.log(`Итераций ${num}`);
+}
+
+
+//Task 8
+
+{
+    // let sum = 0;
+    // let count = 0;
+
+    // while (true) {
+    //     let num = +prompt('Введите число: ');
+    //     if (num === 0) break;
+    //     if (isFinite(num)) {
+    //         sum += num;
+    //         count++;
+    //     } else {
+    //         alert ('Введенные данные не явлляютс числом')
+    //     }
+    // }
+
+    // console.log(sum);
+    // console.log(sum/count);
+}
+
+
+//Task 9
+
+{
+    // let str = '4 98 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 57';
+
+    // let num = str.split(' ');
+
+    // console.log (num);
+    // console.log (Math.max (...num))
+    // console.log (Math.min (...num))
+
+}
